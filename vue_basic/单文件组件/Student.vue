467a8194e726a011f3277f2ecdc94b027d910fe1
TwoBeefly <109075874+TwoBeefly@users.noreply.github.com>
2022-08-04 15:03:52 +0800

    Create Student.vue

---
+++ vue_basic/单文件组件/Student.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="demo">
+     <!-- 组件的结构 -->
+     <h2>学生姓名：{{name}}</h2>
+     <h2>学生年龄：{{age}}</h2>
+    </div>
+</template>
+
+<script>
+    // 组建交互相关的代码（数据、方法等等）
+    export default ({
+        name:'Student',
+        data() {
+            return {
+                name:'Bob',
+                age:'22'
+            }
+        },
+    })
+</script>
