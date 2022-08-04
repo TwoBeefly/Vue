044bf8e0eb3599cef5d0e4467f5ae743bbed439f
TwoBeefly <109075874+TwoBeefly@users.noreply.github.com>
2022-08-04 15:03:42 +0800

    Create App.vue

---
+++ vue_basic/单文件组件/App.vue
@@ -0,0 +1,16 @@
+<template>
+  
+</template>
+
+<script>
+//引入组件
+import School from './School.vue'
+import Student from "./Student.vue";
+export default {
+    name:'App'
+}
+</script>
+
+<style>
+
+</style>
