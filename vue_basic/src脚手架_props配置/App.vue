fb5b2d77e507ab45184b537d6ebf642f74148b01
TwoBeefly <109075874+TwoBeefly@users.noreply.github.com>
2022-08-10 19:13:43 +0800

    Create App.vue

---
+++ vue_basic/src脚手架_props配置/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <!-- 数据传递 -->
+        <Student :age='16' name="pjc" :gender="man"></Student>
+    </div>
+</template>
+
+<script>
+import Student from './components/Student.vue'
+export default {
+    name:'App',
+    components:{
+        Student,
+    }
+}
+</script>
