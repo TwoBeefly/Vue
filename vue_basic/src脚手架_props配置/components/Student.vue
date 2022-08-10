9702d86fa61dd45a6fc4a0950f1891ee3f31013e
TwoBeefly <109075874+TwoBeefly@users.noreply.github.com>
2022-08-10 19:13:44 +0800

    Create Student.vue

---
+++ vue_basic/src脚手架_props配置/components/Student.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>msg</h1>
+        <h2>学生姓名L{{name}}</h2>
+        <h2>学生年龄:{{age}}</h2>
+        <h2>学生性别:{{gender}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Student',
+    data() {
+        return {
+            msg:大学,
+        }
+    },
+    //props:['name','gender','age']  //简单接受
+    //限制接受
+    props:{
+        name:String,
+        age:Number,
+        gender:String
+    }
+}
+</script>
