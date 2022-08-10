76742a36f46a0c5668fd26bb3104776beae9fb99
TwoBeefly <109075874+TwoBeefly@users.noreply.github.com>
2022-08-10 19:13:57 +0800

    Create App.vue

---
+++ vue_basic/src脚手架ref属性分析/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1 v-text="msg" ref="title"></h1>
+        <button @click="show">点击输出上方DOM元素</button>
+        <company></company>
+    </div>
+</template>
+
+<script>
+import Company from './components/Company.vue'
+export default {
+    name:'App',
+    data() {
+        return {
+            msg:"公司老板信息汇总"
+        }
+    },
+    methods: {
+        show(){
+            console.log(this.$refs.title);
+        }
+    },
+    components:{
+        Company,
+    }
+}
+</script>
