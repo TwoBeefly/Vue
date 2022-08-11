4d58883532dca758f240e23f533382a608ed27c8
TwoBeefly <109075874+TwoBeefly@users.noreply.github.com>
2022-08-11 10:55:04 +0800

    Create School.vue

---
+++ vue_basic/src脚手架_scoped样式/components/School.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="demo">
+        <h2>学校名称{{name}}</h2>
+        <h2>学校校龄:{{age}}</h2>
+        <h2>学校地址:{{address}}</h2>
+    </div>
+</template>
+
+<script>
+//引入一个混合
+export default {
+    name:'School',
+    data() {
+        return {
+            name:"金陵小学",
+            age:40,
+            address:'文化路'
+        }
+    }
+}
+</script>
+
+<style scoped>
+.demo{
+    background-color: aquamarine;
+}
+</style>
