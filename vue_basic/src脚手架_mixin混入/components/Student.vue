d7da9a41e81d79e23be49dcb8eb677304bb0fd99
TwoBeefly <109075874+TwoBeefly@users.noreply.github.com>
2022-08-11 10:54:49 +0800

    Create Student.vue

---
+++ vue_basic/src脚手架_mixin混入/components/Student.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2 @click="show">学生姓名{{name}}</h2>
+        <h2>学生年龄:{{age}}</h2>
+        <h2>学生性别:{{gender}}</h2>
+    </div>
+</template>
+
+<script>
+import { mixin } from "../mixin";
+export default {
+    name:'Student',
+    data() {
+        return {
+            name:"pjc",
+            age:22,
+            gender:'man'
+        }
+    },
+    mixins:[mixin]
+}
+</script>
