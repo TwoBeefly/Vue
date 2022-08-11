d06cdd0c211e31f5883455bff600293e1279eedd
TwoBeefly <109075874+TwoBeefly@users.noreply.github.com>
2022-08-11 10:54:47 +0800

    Create School.vue

---
+++ vue_basic/src脚手架_mixin混入/components/School.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2 @click="show">学校名称{{name}}</h2>
+        <h2>学校校龄:{{age}}</h2>
+        <h2>学校地址:{{address}}</h2>
+    </div>
+</template>
+
+<script>
+//引入一个混合
+import { mixin } from "../mixin";
+export default {
+    name:'School',
+    data() {
+        return {
+            name:"金陵小学",
+            age:40,
+            address:'文化路'
+        }
+    },
+    mixins:[mixin]
+}
+</script>
