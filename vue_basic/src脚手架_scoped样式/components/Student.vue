673ddbb57f098d093f537dd4a2c64612dce8394f
TwoBeefly <109075874+TwoBeefly@users.noreply.github.com>
2022-08-11 10:55:08 +0800

    Create Student.vue

---
+++ vue_basic/src脚手架_scoped样式/components/Student.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>学生姓名{{name}}</h2>
+        <h2>学生年龄:{{age}}</h2>
+        <h2>学生性别:{{gender}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Student',
+    data() {
+        return {
+            name:"pjc",
+            age:22,
+            gender:'man'
+        }
+    }
+}
+</script>
