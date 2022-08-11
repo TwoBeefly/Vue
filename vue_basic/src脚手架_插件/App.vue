0d1e09bf66668030b120ea679017a9134fbbf41e
TwoBeefly <109075874+TwoBeefly@users.noreply.github.com>
2022-08-11 10:55:18 +0800

    Create App.vue

---
+++ vue_basic/src脚手架_插件/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <company></company>
+    </div>
+</template>
+
+<script>
+import Company from './components/Company.vue'
+export default {
+    name:'App',
+    components:{
+        Company,
+    }
+}
+</script>
