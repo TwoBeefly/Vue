6bc0564c01707ecfe595a957cc377cf3474b4ad6
TwoBeefly <109075874+TwoBeefly@users.noreply.github.com>
2022-08-11 10:55:20 +0800

    Create Company.vue

---
+++ vue_basic/src脚手架_插件/components/Company.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>BOSS姓名L{{boss}}</h2>
+        <h2>BOSS年龄:{{age}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Company',
+    data() {
+        return {
+            boss:'Peter',
+            age:999
+        }
+    },
+}
+</script>
